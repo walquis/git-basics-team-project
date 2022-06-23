749bb05a139c366ec4511bd1ba2aa597def2b13c
Chris Walquist <cwalquist@drw.com>
2022-06-23 12:43:36 -0500

    Fix some bugs in appending aliases so it works on macs better

--- bin/append-aliases-to-profile.sh
+++ bin/append-aliases-to-profile.sh
@@ -1,9 +1,9 @@
 sh=`basename $SHELL`
-profile=~/.${sh}_profile
+profile=~/.${sh}rc
 if ! grep -l 'alias adog' $profile >> /dev/null
 then
   echo >> $profile  # In case the profile doesn't end with a newline
-  cat `dirname ${BASH_SOURCE[0]}`/git-aliases.sh >> $profile
+  cat bin/git-aliases.sh >> $profile
   echo Appended git aliases to $profile
 else
   echo "Found 'adog' alias already in $profile, assuming the rest are there too."
