3f50c4c70d2ec3da87c1a0f4b348fc5466461421
Chris Walquist <cwalquist@drw.com>
2023-06-13 09:36:44 -0500

    Add adddog (include username in one-line) Add guc - add working tree and index to previous commit, using same comment Make refs for addog and adddog show up in red, just like adog default.

--- bin/git-aliases.sh
+++ bin/git-aliases.sh
@@ -1,13 +1,32 @@
 
-alias adog="git log --all --decorate --oneline --graph"
-alias addog='git log --all --decorate --oneline --graph --pretty=format:"%C(yellow)%h%Creset%C(cyan)%C(bold)%d%Creset %C(cyan)%cd%Creset %s" --date=format:"%y-%m%d-%H%M"'
+alias adog='git   log --all --decorate --oneline --graph'
+
+# Include commit dates
+alias addog='git  log --all --decorate --oneline --graph --pretty=format:"%C(yellow)%h%Creset%C(red)%C(bold)%d%Creset %C(cyan)%cd%Creset %s" --date=format:"%y-%m%d-%H%M"'
+
+# Include login of committer
+alias adddog='git log --all --decorate --oneline --graph --pretty=format:"%C(yellow)%h%Creset %C(magenta)%cl%Creset%C(red)%C(bold)%d%Creset %C(cyan)%cd%Creset %s" --date=format:"%y-%m%d-%H%M"'
+
+alias gs="git status"
 alias ga="git add"
 alias gb="git branch"
 alias gc="git commit"
 alias gco="git checkout"
 alias gcom="git checkout main"
+
+# Diff between working tree and index (staging area)
 alias gd="git diff"
+
+# Diff between index (staging area) and HEAD
 alias gds="git diff --staged"
+
+# List current git config
 alias gl="git config -l"
+
+# What remote am I pointing to?
 alias grso="git remote show origin"
-alias gs="git status"
+
+# Amend previous commit (--amend) to include everything in
+# working tree (-a) and staging area (commit), using the
+# same comment (--no-edit).
+guc='git commit -a --amend --no-edit'
